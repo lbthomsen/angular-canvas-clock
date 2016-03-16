b8bf39b805555e17ee716ce0fe5aba244458b21a
Lars Boegild Thomsen <lth@netcompartner.com>
2016-03-16 12:18:45 +0800

    While crude clock is actually working now.  No face, no decorations, just hour, minute and second hands.

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
 
             $interval(function () {
                 service.now = new Date();
-            }, 1000);
+            }, 100);
 
             return service;
         }
@@ -33,7 +33,12 @@
                 replace: true,
                 scope: true,
                 link: function (scope, element, attrs) {
-                    scope.ctx = element[0].getContext('2d');
+                    scope.canvas = element[0];
+                    scope.context = element[0].getContext('2d');
+
+                    scope.canvas.height = scope.canvas.parentElement.clientHeight;
+                    scope.canvas.width = scope.canvas.parentElement.clientWidth;
+
                     //scope.clockContext.font = "30px Arial";
 
                     //scope.clockContext.fillText("Hello World", 10, 50);
@@ -42,40 +47,65 @@
                     function ($log, $scope, timerService) {
                         $log.debug("Clock Canvas Controller");
                         $log.debug("Scope: %o", $scope);
-                        
+
                         $scope.timerService = timerService;
 
                         $scope.$watch('timerService.now', function () {
-                            
+
                             // Calculate angle
-                            var hourAngle = 0.5 * (60 * timerService.now.getHours() + timerService.now.getMinutes());
-                            var minuteAngle = 6 * timerService.now.getMinutes();
-                            var secondAngle = 6 * timerService.now.getSeconds() + 6 / 1000 * timerService.now.getMilliseconds();
-                            
+                            var hourAngle = 0.5 * (60 * timerService.now.getHours() + timerService.now.getMinutes()) - 90;
+                            var minuteAngle = 6 * timerService.now.getMinutes() - 90;
+                            var secondAngle = 6 * timerService.now.getSeconds() + 6 / 1000 * timerService.now.getMilliseconds() - 90;
+
                             //$log.debug("Angles: hour=%d minute=%d second=%d", hourAngle, minuteAngle, secondAngle);
-                            
+
                             // So far so good - we can now calculate coordinates
-                            newHourHand = {
-                                a: hourAngle, 
-                                x: Math.cos(hourAngle * Math.PI / 180), 
-                                y: Math.sin(hourAngle * Math.PI / 180)
+                            var hourHandLength = Math.floor($scope.canvas.width * 0.2);
+                            var minuteHandLength = Math.floor($scope.canvas.width * 0.4);
+                            var secondHandLength = Math.floor($scope.canvas.width * 0.47);
+                            
+                            var center = {
+                                x: Math.floor($scope.canvas.width / 2),
+                                y: Math.floor($scope.canvas.height / 2)
+                            };                            
+
+                            var newHourHand = {
+                                a: hourAngle,
+                                x: Math.cos(hourAngle * Math.PI / 180) * hourHandLength + center.x,
+                                y: Math.sin(hourAngle * Math.PI / 180) * hourHandLength + center.y
                             };
-                            
-                            newMinuteHand = {
-                                a: minuteAngle, 
-                                x: Math.cos(minuteAngle * Math.PI / 180), 
-                                y: Math.sin(minuteAngle * Math.PI / 180)
+
+                            var newMinuteHand = {
+                                a: minuteAngle,
+                                x: Math.cos(minuteAngle * Math.PI / 180) * minuteHandLength + center.x,
+                                y: Math.sin(minuteAngle * Math.PI / 180) * minuteHandLength + center.y
                             };
-                            
-                            newSecondHand = {
-                                a: secondAngle, 
-                                x: Math.cos(minuteAngle * Math.PI / 180), 
-                                y: Math.sin(minuteAngle * Math.PI / 180)
+
+                            var newSecondHand = {
+                                a: secondAngle,
+                                x: Math.cos(secondAngle * Math.PI / 180) * secondHandLength + center.x,
+                                y: Math.sin(secondAngle * Math.PI / 180) * secondHandLength + center.y
                             };
+
+                            // $log.debug("Objects: %o %o %o", newHourHand, newMinuteHand, newSecondHand, center);
                             
-                            $log.debug("Objects: %o %o %o", newHourHand, newMinuteHand, newSecondHand);
+                            $scope.context.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
                             
-                            $log.debug("Tick");
+                            $scope.context.beginPath();
+                            $scope.context.moveTo(center.x, center.y);
+                            $scope.context.lineTo(newHourHand.x, newHourHand.y);
+                            $scope.context.stroke();
+
+                            $scope.context.beginPath();
+                            $scope.context.moveTo(center.x, center.y);
+                            $scope.context.lineTo(newMinuteHand.x, newMinuteHand.y);
+                            $scope.context.stroke();
+
+                            $scope.context.beginPath();
+                            $scope.context.moveTo(center.x, center.y);
+                            $scope.context.lineTo(newSecondHand.x, newSecondHand.y);
+                            $scope.context.stroke();
+
                         });
 
                     }
